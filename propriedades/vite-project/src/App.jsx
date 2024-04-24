d95d022fed900df6f8ad32d4f68e9f0bdef8c5e3
Márcio César <marcinho060498@gmail.com>
2024-04-24 16:41:51 -0300

    aula sobre propriedades jsx

---
+++ propriedades/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Titulo from "./Titulos"
+const App = ()=> {
+
+
+
+
+  return (
+    <>
+    <Titulo cor="red" text="Meu titulo 1"/>
+    <Titulo cor="blue" text="Meu titulo 2"/>
+    <Titulo text="Meu titulo 3"/>
+    </>
+  )
+
+}
+
+export default App
