09c0ca068c945a6d7ccb8764e170e01ce3d81afb
Márcio César <marcinho060498@gmail.com>
2024-04-29 21:10:30 -0300

    Aprendendo sobre propriedades

--- propriedades/vite-project/src/App.jsx
+++ propriedades/vite-project/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Titulo from "./Titulos"
+import Form from "./Form/Form"
 const App = ()=> {
 
 
@@ -10,6 +11,8 @@ const App = ()=> {
     <Titulo cor="red" text="Meu titulo 1"/>
     <Titulo cor="blue" text="Meu titulo 2"/>
     <Titulo text="Meu titulo 3"/>
+
+    <Form />
     </>
   )
 
