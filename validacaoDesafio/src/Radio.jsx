cf11a025819230adb40378b7f69fcf456a4d8a08
Márcio César <marcinho060498@gmail.com>
2024-05-25 17:04:21 -0300

    Desafio FORMUlários

---
+++ validacaoDesafio/src/Radio.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Radio = ({pergunta, options, id, onChange, value, active}) => {
+  if(active === false) return null;
+  return (
+    <fieldset>
+      <legend id={id}>{pergunta}</legend>
+
+      {options.map((option)=>(
+        <label key={option}>
+          <input type="radio" 
+          id={id}
+          checked={value === option} 
+          value={option} 
+          onChange={onChange}/>
+          {option}
+        </label>
+      ))
+    }
+    </fieldset>
+  );
+};
+
+export default Radio
