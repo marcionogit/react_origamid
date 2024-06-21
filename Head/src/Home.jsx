d54eb78dd6b05878cc99786d8682387d1a5ee4a3
Márcio César <marcinho060498@gmail.com>
2024-06-20 23:29:50 -0300

    Head

---
+++ Head/src/Home.jsx
@@ -0,0 +1,14 @@
+import Head from './Head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head title="Página Home" description="Descrição da home" />
+      <h1>Home</h1>
+      <p>Essa é a home</p>
+    </div>
+  );
+};
+
+
+export default Home;
