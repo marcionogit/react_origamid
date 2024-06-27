44e058f062fd732251cfd24a98701ef6daa3ee00
Márcio César <marcinho060498@gmail.com>
2024-06-27 00:43:43 -0300

    Desafio Router part 2

--- desafioRouter/src/App.jsx
+++ desafioRouter/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Produtos from "./Components/Produtos";
+import Produto from "./Components/Produto";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Contato from "./Components/Contato";
@@ -18,6 +19,7 @@ const App = () => {
         <div className="content">
           <Routes>
             <Route path="/" element={<Produtos />} />
+            <Route path="produto/:id" element={<Produto />} />
             <Route path="contato" element={<Contato />} />
           </Routes>
         </div>
---
+++ desafioRouter/src/Components/Produto.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styles from './Produto.module.css';
+import {useParams} from 'react-router-dom';
+
+const Produto = () => {
+  const {id} = useParams();
+  const [produto, setProduto] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(()=>{
+    async function fetchProduto(url){
+      try{
+        setLoading(true)
+        const response = await fetch(url);
+        const json = await response.json();
+        setProduto(json)
+      } catch(erro){
+        setError('Um erro ocorreu.')
+      } finally{
+        setLoading(false)
+      }
+  
+    }
+    fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`)
+
+
+  }, [id])
+
+  if(loading) return <p>Carregando...</p>;
+  if(error) return error;
+  if(produto === null)  return null;
+  return (
+    <section className={`${styles.produto} animaLeft`}>
+      {produto.fotos.map( foto =>(
+        <img key={foto.src} src={foto.src} alt={foto.titulo} />
+      ))}
+      <div>
+        <h1>{produto.nome}</h1>
+        <span className={styles.preco}>R$ {produto.preco}</span>
+        <p className={styles.descricao}>{produto.descricao}</p>
+      </div>
+    </section>
+  )
+}
+
+export default Produto
