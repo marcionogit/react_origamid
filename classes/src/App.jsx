ec8bb9212be8da8e188e10d34b8fe3e2619c0cef
Márcio César <marcinho060498@gmail.com>
2024-07-20 15:55:17 -0300

    Classes / No momento não executei a aula por recomendação do professor. Apenas assisti.

---
+++ classes/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Olá, Mundo!</h1>
+      <p>Não fiz a aula no momento, pois não iria utilizar no momento</p>
+    </>
+  )
+}
+
+export default App
